9f1b236a1b7d9fb86a8dce514d36b094fd9e1e3a
8baller <tomrae030@hotmail.com>
2023-06-01 14:03:19 +0100

    [fix] linters

--- rysk_client/src/utils.py
+++ rysk_client/src/utils.py
@@ -8,12 +8,10 @@ from copy import deepcopy
 from web3 import Web3
 
 from rysk_client.src.constants import ADDRESSES, DEFAULT_ENCODING, RPC_URL
-# 
 
 
 def get_contract(name, web3):
     """Returns a web3 contract instance for the given contract name"""
-    # we need to set the root of the path to be the installed rysk package
     path = os.path.join(os.path.dirname(__file__), "..")
     spec = ADDRESSES[name]
     with open(f"{path}/{spec['path']}", "r", encoding=DEFAULT_ENCODING) as abi:
