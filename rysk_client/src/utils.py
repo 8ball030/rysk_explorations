ff0d9f1b9bf031415653b7946f9ceb531c63c7c4
8ball030 <35799987+8ball030@users.noreply.github.com>
2023-06-01 13:59:23 +0100

    V0.1.1 (#14)
    * [admin] first release
    
    * [admin] first release
    
    * [feat] added dist to ignore
    
    * [feat] moved contracts
    
    * Bump version: 0.1.0 → 0.1.1
    
    * [feat] bumped to 0.1.1
    
    * Bump version: 0.1.1 → 0.1.2
    
    * Bump version: 0.1.2 → 0.1.1

--- rysk_client/src/utils.py
+++ rysk_client/src/utils.py
@@ -2,17 +2,21 @@
 Helper functions for the rysk_client package.
 """
 import json
+import os
 from copy import deepcopy
 
 from web3 import Web3
 
 from rysk_client.src.constants import ADDRESSES, DEFAULT_ENCODING, RPC_URL
+# 
 
 
 def get_contract(name, web3):
     """Returns a web3 contract instance for the given contract name"""
+    # we need to set the root of the path to be the installed rysk package
+    path = os.path.join(os.path.dirname(__file__), "..")
     spec = ADDRESSES[name]
-    with open(spec["path"], "r", encoding=DEFAULT_ENCODING) as abi:
+    with open(f"{path}/{spec['path']}", "r", encoding=DEFAULT_ENCODING) as abi:
         abi = json.loads(abi.read())["abi"]
     res = deepcopy(spec)
     del res["path"]
