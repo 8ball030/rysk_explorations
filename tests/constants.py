74d82d5041a2995bc0480cf58548dbef6db10a3e
8baller <tomrae030@hotmail.com>
2023-08-21 23:44:01 +0100

    [feat] refactors the constants

--- tests/constants.py
+++ tests/constants.py
@@ -6,4 +6,4 @@ DEFAULT_PRIVATE_KEY = (
     "0x75cc9212e9e1243b9a3e5db5012f39469254088e33363324ad94dd0b212d7efa"
 )
 DEFAULT_FORK_BLOCK_NUMBER = 30495873
-TESTNET_RPC_URL = "https://arbitrum-goerli.rpc.thirdweb.com"
+TESTNET_RPC_URL = "https://endpoints.omniatech.io/v1/arbitrum/goerli/public"
